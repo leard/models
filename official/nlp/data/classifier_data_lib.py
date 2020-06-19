8f22026c2bafeb8f7f8194766fd1a22522d3bd52
Leard de Oliveira Fernandes <leard@users.noreply.github.com>
2020-06-19 10:18:07 -0300

    Adjust for new inputs

--- official/nlp/data/classifier_data_lib.py
+++ official/nlp/data/classifier_data_lib.py
@@ -246,6 +246,7 @@ class NliProcessor(DataProcessor):
           InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
     return examples
 
+
 class MnliProcessor(DataProcessor):
   """Processor for the MultiNLI data set (GLUE version)."""
 
--- official/nlp/data/create_finetuning_data.py
+++ official/nlp/data/create_finetuning_data.py
@@ -45,7 +45,7 @@ flags.DEFINE_string(
     "for the task.")
 
 flags.DEFINE_enum("classification_task_name", "MNLI",
-                  ["COLA", "MNLI", "MRPC", "QNLI", "SST-2", "XNLI"],
+                  ["COLA", "MNLI", "MRPC", "QNLI", "SST-2", "XNLI", "NLI","ASSIN"],
                   "The name of the task to train BERT classifier.")
 
 # BERT Squad task specific flags.
@@ -116,6 +116,8 @@ def generate_classifier_dataset():
       "qnli": classifier_data_lib.QnliProcessor,
       "sst-2": classifier_data_lib.SstProcessor,
       "xnli": classifier_data_lib.XnliProcessor,
+      "nli": classifier_data_lib.NliProcessor,
+      "assin": classifier_data_lib.AssinProcessor,
   }
   task_name = FLAGS.classification_task_name.lower()
   if task_name not in processors:
