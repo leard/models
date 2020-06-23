a0a8ced7dd112fb1fd9c845e93fd5b08f704009e
Leard de Oliveira Fernandes <leard@users.noreply.github.com>
2020-06-23 14:09:57 -0300

    Update run_classifier.py

--- official/nlp/bert/run_classifier.py
+++ official/nlp/bert/run_classifier.py
@@ -384,8 +384,8 @@ def run_bert(strategy,
     output_predict_file = os.path.join(FLAGS.model_dir, 'test_results.tsv')
     with tf.io.gfile.GFile(output_predict_file, 'w') as writer:
       logging.info('***** Predict results *****')
-      for probabilities, label in zip(preds, labels):
-        output_line = '\t' + str(probabilities) + '\t' + label + '\n'
+      for probabilities in preds:
+        output_line = '\t' + str(probabilities) + '\n'
         writer.write(output_line)
     return
 
