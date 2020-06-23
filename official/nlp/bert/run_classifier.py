644fab349fd643503ee7997909c43b46f5440cd7
Leard de Oliveira Fernandes <leard@users.noreply.github.com>
2020-06-23 18:58:06 -0300

    Update run_classifier.py

--- official/nlp/bert/run_classifier.py
+++ official/nlp/bert/run_classifier.py
@@ -399,26 +399,32 @@ def run_bert(strategy,
                   logging.info(f'word_embeddings getting weights: {transformer_sub_layer.name}')
                   word_embeddings_weights = pretrain_model.layers[i].layers[j].layers[k].get_weights()
 
-
     assert word_embeddings_weights
 
-    # classifier_model, bert_model = bert_models.classifier_model(model_config,
-    #                                                             input_meta_data['num_labels'],
-    #                                                             input_meta_data['max_seq_length'])[0]
-    # checkpoint = tf.train.Checkpoint(model=bert_model)
-    # latest_checkpoint_file = tf.train.latest_checkpoint(FLAGS.model_dir)
-    # assert latest_checkpoint_file
-    # logging.info('Checkpoint file %s found and restoring from '
-    #              'checkpoint', latest_checkpoint_file)
-    # checkpoint.restore(latest_checkpoint_file).assert_existing_objects_matched() #.expect_partial()
+    classifier_model, bert_model = bert_models.classifier_model(model_config,
+                                                                input_meta_data['num_labels'],
+                                                                input_meta_data['max_seq_length'])[0]
+    checkpoint = tf.train.Checkpoint(model=bert_model)
+    latest_checkpoint_file = tf.train.latest_checkpoint(FLAGS.model_dir)
+    assert latest_checkpoint_file
+    logging.info('Checkpoint file %s found and restoring from '
+                 'checkpoint', latest_checkpoint_file)
+    checkpoint.restore(latest_checkpoint_file).assert_existing_objects_matched() #.expect_partial()
+    logging.info(classifier_model.summary())
+    logging.info(bert_model.summary())
 
     # for i, layer in enumerate(classifier_model.layers):
-    #     if 'transformer_encoder' in layer.name:
-    #       for k, transformer_sub_layer in enumerate(layer.layers):
-    #         if 'word_embeddings' in transformer_sub_layer.name:
-    #           pretrainer_model.layers[i].layers[j].set_weights(word_embeddings_weights)
-    #           #logging.info(f'setting word_embeddings: {transformer_sub_layer.name}')
-    #           break
+    #   if 'bert_pretrainer' in layer.name:
+    #     bert_pretrainer_layer = layer
+    #     logging.info(f'Layer Name: {bert_pretrainer_layer.name}')
+    #     for j, bert_sub_layer in enumerate(bert_pretrainer_layer.layers):
+    #       if 'transformer_encoder' in bert_sub_layer.name:
+    #         logging.info(f'#bert_sub_layer: {bert_sub_layer.name}')
+    #         transformer_encoder_layer = bert_sub_layer
+    #         for k, transformer_sub_layer in enumerate(transformer_encoder_layer.layers):
+    #           if 'word_embeddings' in transformer_sub_layer.name:
+    #             logging.info(f'word_embeddings getting weights: {transformer_sub_layer.name}')
+    #             pretrain_model.layers[i].layers[j].layers[k].set_weights(word_embeddings_weights)
     #
     # logging.info('######Summary classifier_model######')
     # #logging.info(classifier_model.summary())
