4b6006c1d394eda6dc0bc92596154219f2df8d1b
Leard de Oliveira Fernandes <leard@users.noreply.github.com>
2020-06-24 12:05:05 -0300

    Update run_classifier.py

--- official/nlp/bert/run_classifier.py
+++ official/nlp/bert/run_classifier.py
@@ -415,8 +415,8 @@ def run_bert(strategy,
                                                   input_meta_data['max_seq_length'])
 
     logging.info(f'classifier_model before:{classifier_model.layers[3].layers[1].get_weights()[0]}')
-
-    checkpoint = tf.train.Checkpoint(model=bert_model)
+    logging.info(bert_model.summary())
+    checkpoint = tf.train.Checkpoint(model=classifier_model)
     latest_checkpoint_file = tf.train.latest_checkpoint(FLAGS.model_dir)
     assert latest_checkpoint_file
     logging.info('Checkpoint file %s found and restoring from '
